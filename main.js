ac14ebc0da856c4bd19a526e9856a6bc67d8ef30
mraq123 <hairaffi2@gmail.com>
2020-10-02 23:08:18 +0700

    update showcase page

--- main.js
+++ main.js
@@ -18,3 +18,27 @@ const closeModal = () => {
     modal.style.display = "none"
 
 }
+
+// kita dapatkan dulu element yang lu mau bro
+let showcase = document.getElementById("wrapper_showcase")
+
+
+// kita inject elemennya dengan syntax HTML
+// funsi += untuk menambahkan element biar engga niban
+// inner untuk membuat html bukan text doang
+// showcase.innerHTML += `
+// <img src="https://picsum.photos/seed/66/300"/>
+
+// `
+
+for (var i = 0; i < 40; i++) {
+    showcase.innerHTML += `
+        <img src="https://picsum.photos/seed/${30 + i}/150" alt="lolabang ${i}"/>
+
+    `
+
+}
+
+
+
+
